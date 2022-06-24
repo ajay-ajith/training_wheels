29fac9799a452e58b79516c03477a5f5e6a728c2
ajay-ajith <drax0x01.dev@gmail.com>
2022-06-24 13:51:55 +0530

    added examples

---
+++ goroutines_and_channels/channels/example-2/main.go
@@ -0,0 +1,30 @@
+// An example of using goroutines along with a channel
+// A goroutine producer will produce the N'th number in the series and writes it to the channel
+// The consumer will wait for M seconds before stopping the program
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	result := make(chan int)
+
+	go func(limit int) {
+		a, b := 0, 1
+		for i := 0; i < limit-1; i++ {
+			a, b = b, a+b
+		}
+		result <- a
+	}(1000)
+
+	select {
+	case res := <-result:
+		fmt.Println(res)
+	case <-time.After(1 / 10 * time.Second):
+		fmt.Println("Exitting...")
+	}
+
+}
